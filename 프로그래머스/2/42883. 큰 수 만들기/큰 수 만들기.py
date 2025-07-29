6758133b562274a7f52d4e5f63a97f6cf626e1df
keemgdeok <keemgdeok@gmail.com>
2025-07-29 23:50:51 +0900

    [level 2] Title: 큰 수 만들기, Time: 96.45 ms, Memory: 11.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -0,0 +1,19 @@
+import itertools
+
+def solution(number, k):
+    ans = ''
+    stack = []
+    
+    for n in number:
+        while stack and k > 0 and stack[-1] < n:
+            stack.pop()
+            k-=1
+        stack.append(n)
+    
+    if k > 0:
+        stack = stack[:-k]
+
+    return ''.join(stack)
+    
+        
+            
