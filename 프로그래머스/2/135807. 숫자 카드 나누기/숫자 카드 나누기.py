f02b0c702d26a7c5ed712aa002ed7b8c32c3ca04
keemgdeok <keemgdeok@gmail.com>
2025-08-25 16:22:52 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 248.36 ms, Memory: 52.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.py
@@ -0,0 +1,35 @@
+import math
+
+def solution(arrayA, arrayB):
+    ans = 0
+    # find 철수&영희의 최대공약수
+    A = findGCD(arrayA)
+    B = findGCD(arrayB)
+    print(A, B)
+
+    if A>1:
+        for b in arrayB:
+            if b%A==0:
+                A=0
+                break
+    
+    if B>1:
+        for a in arrayA:
+            if a%B==0:
+                B=0
+                break
+                
+    if A==1:
+        A=0
+    if B==1:
+        B=0
+    
+    return max(A, B)
+
+def findGCD(cards):
+    result = cards[0]
+    for i in range(1, len(cards)):
+        result = math.gcd(result, cards[i])
+    
+    return result
+
