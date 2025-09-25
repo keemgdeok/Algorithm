8cf5e6d70d27ad97587c5647da5719a112713a89
gyeongdeok kim <keemgdeok@gmail.com>
2025-09-25 16:15:13 +0900

    [level 2] Title: 점 찍기, Time: 771.46 ms, Memory: 9.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.py
@@ -0,0 +1,10 @@
+import math
+
+def solution(k, d):
+    answer = 0
+    for x in range(0, d+1, k):
+        max_y = math.floor(math.sqrt(d**2 - x**2))
+        answer += (max_y // k) + 1
+    
+    return answer
+
