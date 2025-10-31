0b1b4a3046a143348966039632be617f6c2a3806
gyeongdeok kim <keemgdeok@gmail.com>
2025-10-31 17:05:37 +0900

    [Gold III] Title: 불!, Time: 288 ms, Memory: 133536 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.py
+++ 백준/Gold/4179. 불！/불！.py
@@ -4,66 +4,51 @@ input = sys.stdin.readline
 
 # input
 R, C = map(int, input().split())
-maze = [[*input().strip()] for _ in range(R)]
+maze = [input().rstrip() for _ in range(R)]
 
-"""
-requirement : 
-지훈이가 불에 타기전에 탈출할 수 있는지의 여부,
-얼마나 빨리 탈출할 수 있는지를 결정
+dq = deque()
+ji = [-1, -1]
+visited = [[-1]*C for _ in range(R)]
+for i in range(R):
+    for j in range(C):
+        if maze[i][j] == "J":
+            ji = [i, j]
+        elif maze[i][j] == "F":
+            dq.append((i,j,0))
+            visited[i][j] = -2
+        elif maze[i][j] == "#":
+            visited[i][j] = -2
 
-point :
-2번의 BFS
-first : 지훈이가 가장자리 까지 도달하는 BFS 
-second : 불이 퍼지는 BFS
+dq.append((*ji, 1))
+visited[ji[0]][ji[1]] = 1
 
-first + second 의 가장자리를 비교하여 값 도출
-"""
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
 
-escaped = [[0]*C for _ in range(R)]
-INF = float('INF')
-fired = [[INF]*C for _ in range(R)]
-
-start_fired=[]
-for y in range(R):
-    for x in range(C):
-        if maze[y][x] == 'J':
-            start_jh = (y, x)            
-        elif maze[y][x] == "F":
-            start_fired.append((y, x))
-        
-# BFS 시작
-dy = [0, 0, -1, 1]
-dx = [1, -1, 0, 0]
-
-# fire
-fired_dq = deque()
-for y, x in start_fired:
-    fired_dq.append((y,x))
-    fired[y][x] = 1
-
-while fired_dq:
-    y, x = fired_dq.popleft()
+while dq:
+    y, x, stat = dq.popleft()
     for i in range(4):
-        ny = y + dy[i]; nx = x + dx[i]
-        if 0<=ny<R and 0<=nx<C and maze[ny][nx] != "#" and fired[ny][nx]==INF:
-            fired[ny][nx] = fired[y][x] + 1
-            fired_dq.append((ny, nx))
-            
-# Jihoon
-jh_dq = deque()
-jh_dq.append(start_jh)
-escaped[start_jh[0]][start_jh[1]]=1
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if (nx<0 or nx>=C or ny<0 or ny>=R): continue
 
-while jh_dq:
-    y, x = jh_dq.popleft()
-    if y==0 or y==R-1 or x==0 or x==C-1:
-        print(escaped[y][x]); exit()
-    for i in range(4):
-        ny = y + dy[i]; nx = x + dx[i]
-        if 0<=ny<R and 0<=nx<C and maze[ny][nx] == "." and not escaped[ny][nx] and escaped[y][x] + 1 < fired[ny][nx]:
-            jh_dq.append((ny,nx))
-            escaped[ny][nx] = escaped[y][x] + 1
+        if stat==0:
+            if visited[ny][nx] == -1:
+                visited[ny][nx] = -2
+                dq.append((ny, nx, stat))
+        else:
+            if visited[ny][nx] == -1:
+                visited[ny][nx] = visited[y][x] + 1
+                dq.append((ny, nx, stat))
 
-print("IMPOSSIBLE")   
-    
+ans = int(1e9)
+for i in range(R):
+    if visited[i][0] > 0: ans = min(ans, visited[i][0])
+    if visited[i][-1] > 0: ans = min(ans, visited[i][-1])
+
+for j in range(C):
+    if visited[0][j] > 0: ans = min(ans, visited[0][j])
+    if visited[-1][j] > 0: ans = min(ans, visited[-1][j])
+
+print(ans if ans != int(1e9) else "IMPOSSIBLE")
 
