a68cae4567b93e8738eb7e5e0f3010809f677dd6
gyeongdeok kim <keemgdeok@gmail.com>
2025-12-11 16:24:36 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,15 @@
+select
+    concat("/home/grep/src/", b.board_id, "/", f.file_id, f.file_name, f.file_ext) as FILE_PATH
+from
+    USED_GOODS_BOARD as b
+join
+    USED_GOODS_FILE as f
+    on b.board_id = f.board_id
+where
+    b.views = (select
+                max(views)
+              from
+                USED_GOODS_BOARD
+              )
+order by
+    f.file_id desc
