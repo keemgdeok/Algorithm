a78a0a16647347e77539963bbc30e684c0a32691
gyeongdeok kim <keemgdeok@gmail.com>
2026-01-14 21:47:35 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -3,12 +3,15 @@ select
     ins.animal_type as ANIMAL_TYPE,
     ins.name as NAME
 from
-    animal_ins ins
+    ANIMAL_INS ins
 join
-    animal_outs outs
+    ANIMAL_OUTS outs
     on ins.animal_id = outs.animal_id
 where
-    ins.sex_upon_intake like 'Intact%'
-    and outs.sex_upon_outcome not like 'Intact%'
+    ins.sex_upon_intake like "Intact%"
+    and
+    (outs.sex_upon_outcome like "Spayed%" or
+    outs.sex_upon_outcome like "Neutered%")
 order by
-    ANIMAL_ID asc
+    ins.animal_id asc
+    
