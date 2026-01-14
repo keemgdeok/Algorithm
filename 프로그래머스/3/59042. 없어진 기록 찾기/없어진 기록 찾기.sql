0e69a00353428a4cb674b7d951ce789525754765
gyeongdeok kim <keemgdeok@gmail.com>
2026-01-14 21:19:11 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -2,10 +2,9 @@ select
     outs.animal_id as ANIMAL_ID,
     outs.name as NAME
 from
-    animal_ins as ins
+    ANIMAL_INS ins
 right join
-    animal_outs as outs
+    ANIMAL_OUTS outs
     on ins.animal_id = outs.animal_id
 where
     ins.animal_id is null
-    
